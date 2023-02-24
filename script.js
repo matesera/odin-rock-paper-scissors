fcbad1d432fbead37ce684ba0706465dd1898269
matesera <87706897+matesera@users.noreply.github.com>
2023-02-24 16:57:41 +0100

    made the final game function, rps game works as intended

--- script.js
+++ script.js
@@ -8,7 +8,18 @@ function getComputerChoice () {
 // a function that plays a round of rps, playRound(playerSelection, computerSelection)
 // returns!!! a string with the winner: win-lose, this beats that
 // playerSelection case-insensitive, so it accepts R,r
+
+let playerSelection = '';
+let computerSelection = '';
+let round;
+
 function playRound (playerSelection, computerSelection) {
+
+    let userChoice = prompt("Rock paper scissors shoot: ");
+    playerSelection = userChoice.toLowerCase();
+    computerSelection = getComputerChoice().toLowerCase();
+    
+
     if (playerSelection === 'rock' & computerSelection === 'paper') {
         return "You lose! Paper beats Rock!";
     } else if (playerSelection === 'paper' & computerSelection === 'scissors') {
@@ -21,14 +32,17 @@ function playRound (playerSelection, computerSelection) {
         return "You win! Paper beats Rock!";
     } else if (playerSelection === 'scissors' & computerSelection === 'paper') {
         return "You win! Scissors beats Paper!";
+    } else if (playerSelection === 'rock' & computerSelection === 'rock') {
+        return "Rock on Rock! It's a tie!";
+    } else if (playerSelection === 'paper' & computerSelection === 'paper') {
+        return "Paper on Paper! It's a tie!";
+    } else if (playerSelection === 'scissors' & computerSelection === 'scissors') {
+        return "Scissors on Scissors! It's a tie!";
     } else {
         return "Try again!";
     }
 }
 
-let userChoice = prompt("Rock paper scissors shoot: ");
-let playerSelection = userChoice.toLowerCase();
-let computerSelection = getComputerChoice().toLowerCase();
 
 
 
@@ -37,3 +51,41 @@ let computerSelection = getComputerChoice().toLowerCase();
 
 //a function called game, plays 5 rounds, keeps the score of each and outputs them at the end of each round
 //outputs the winner at the end
+function game () {
+
+    let winScore = 0;
+    let loseScore = 0;
+    let tieScore = 0;
+    
+    for (let i = 0; i < 5; i++) {
+        round = playRound (playerSelection, computerSelection);
+        console.log(round);
+        if (round.includes("win")) {
+            winScore++;
+        } else if (round.includes("lose")) {
+            loseScore++;
+        } else if (round.includes("tie")) {
+            tieScore++;
+        }
+    }
+    
+    
+    if (winScore > loseScore) {
+        console.log("You win! Congrats!");
+    } else if (winScore < loseScore) {
+        console.log("You lose! Computer wins!");
+    } else if (winScore === 0) {
+        console.log("Devastating loss!");
+    } else if (loseScore === 0) {
+        console.log("Flawless victory!");
+    } else if (tieScore > winScore & loseScore) {
+        console.log("It's a tie!"); 
+    } else {
+        console.log("Try again!");
+    }
+}
+
+let test = game();
+console.log(test);
+
+
